b59ae61d8f3bda34e1bb990d1cc75b0212b33c87
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-02-06 17:39:06 -0600

    Impacto de Covid en los Condados de los 4 finalistas de Conferencia

---
+++ Covid-County.r
@@ -0,0 +1,8 @@
+##### Aumento de COVID por Condado
+
+# Esto es necesario para saber como influyo el COVID 19 en el desempeno de los equipos de Baseball
+#### Importamos el CSV.
+
+url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+covidByCounty <- read.csv(file = url)
+
